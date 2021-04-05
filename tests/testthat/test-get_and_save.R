0711e4b6266251f697927614b8e8432c708ac20f
Rohan Alexander <RohanAlexander@users.noreply.github.com>
2021-04-04 21:03:54 -0400

    Add another test

--- tests/testthat/test-get_and_save.R
+++ tests/testthat/test-get_and_save.R
@@ -20,6 +20,31 @@ test_that("get_and_save errors if passed a character for print_every", {
       print_every = "swim_so_wild_and_you_swim_so_free"
       ),
     "The specified value to print_every"
-  )
+    )
   }
   )
+
+test_that("get_and_save downloads a paper if the circumstances are correct", {
+
+  skip_if_offline()
+  skip_on_cran()
+
+  one_pdf <-
+    tibble::tibble(
+      locations_are = c("https://osf.io/preprints/socarxiv/z4qg9/download"),
+      save_here = c("competing_effects_on_the_average_age_of_infant_death.pdf")
+    )
+
+  heapsofpapers::get_and_save(
+    data = one_pdf,
+    links = "locations_are",
+    save_names = "save_here"
+  )
+
+  check_downloaded <- file.exists("./competing_effects_on_the_average_age_of_infant_death.pdf")
+  file.remove("./competing_effects_on_the_average_age_of_infant_death.pdf")
+  expect_equal(check_downloaded, TRUE)
+
+}
+)
+
